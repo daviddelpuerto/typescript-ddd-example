3c24185000306fca048449ac02d119e18d46597d
David del Puerto <code@daviddelpuerto.dev>
2022-09-16 23:01:32 +0200

    Installed and configured eslint with airbnb style guide and created lint script

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-typescript/base',
+  plugins: [
+    'import', 
+    'prettier',
+  ],
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+};
+  
