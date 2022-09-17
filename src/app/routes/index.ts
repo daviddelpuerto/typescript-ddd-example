37cb23a914acb216a81379b302296c0178829a89
David del Puerto <code@daviddelpuerto.dev>
2022-09-17 14:58:55 +0200

    Registered users routes on api router

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import httpStatus from 'http-status-codes';
+import usersRoutes from './users.routes';
 
 const router = Router();
 
 router.get('/api', (_req, res) => {
   return res.sendStatus(httpStatus.OK);
 });
+
+router.use('/api/users', usersRoutes);
 
 export default router;
