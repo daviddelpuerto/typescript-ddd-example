1918ea944bc1088fdaf41acff90c9839c8c828a6
David del Puerto <code@daviddelpuerto.dev>
2022-09-20 22:56:29 +0200

    User UserCreator on CreateUserController to create new user

--- src/modules/Users/infrastructure/controllers/CreateUserController.ts
+++ src/modules/Users/infrastructure/controllers/CreateUserController.ts
@@ -1,15 +1,28 @@
-import { Request, Response } from 'express';
 import httpStatus from 'http-status-codes';
-import { Service } from 'typedi';
+import { Request, Response } from 'express';
 import { Controller } from '../../../../Shared/infrastructure/Controller';
+import UserCreator from '../../application/UserCreator';
 
-@Service('Controllers.CreateUserController')
 export default class CreateUserController implements Controller {
+  public userCreator: UserCreator;
 
-  async run(_req: Request, res: Response) {
+  constructor(userCreator: UserCreator) {
+    this.userCreator = userCreator;
+  }
+
+  async run(req: Request, res: Response) {
     try {
+
+      const { user_email: email, password } = req.body;
+
+      await this.userCreator.run({
+        email,
+        password,
+      });
+
       res.sendStatus(httpStatus.CREATED);
     } catch (error) {
+      console.error(error);
       res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
     }
   }
