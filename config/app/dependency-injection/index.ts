f7a009c8093a011e850322fb0e81f3cc86587142
David del Puerto <code@daviddelpuerto.dev>
2022-09-20 22:58:20 +0200

    Register dependencies using injectDependencies() function

--- config/app/dependency-injection/index.ts
+++ config/app/dependency-injection/index.ts
@@ -1,8 +1,20 @@
-/**
- * @HINT Import Classes decorated with @Service here
- */
+import { Container } from 'typedi';
+import dependencies from './dependencies';
+import WinstonLogger from '../../../src/Shared/infrastructure/Logger';
 
-import '../../../src/Shared/infrastructure/Logger';
+const logger = new WinstonLogger('Dependency-Injection');
 
-// Controllers
-import '../../../src/modules/Users/infrastructure/controllers';
+export default function injectDependencies() {
+  logger.info('💉 Starting dependency injection');
+
+  Object.entries(dependencies).forEach(([key, value]) => {
+    const dependencyEntries = Object.entries(value);
+    
+    dependencyEntries.forEach(([entryKey, entryValue]) => {
+      const dependencyKey = `${key}.${entryKey}`;
+
+      Container.set(dependencyKey, entryValue);
+      logger.info(`✔️  Registered {${dependencyKey}}`);
+    }); 
+  });
+}
