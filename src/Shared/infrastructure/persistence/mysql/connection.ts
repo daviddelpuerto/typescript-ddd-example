d46f27611027dc4b53b96c8d5a135e9019d300fd
David del Puerto <code@daviddelpuerto.dev>
2022-09-29 23:44:41 +0200

    Export function to create database connection

--- src/Shared/infrastructure/persistence/mysql/connection.ts
+++ src/Shared/infrastructure/persistence/mysql/connection.ts
@@ -1,6 +1,9 @@
 import { DataSource } from 'typeorm';
 import config from './config';
 import UserEntity from './entities/User.entity';
+import SharedLogger from '../../Logger';
+
+const logger = new SharedLogger('Database');
 
 const {
   database,
@@ -23,3 +26,21 @@ export const AppDataSource = new DataSource({
   subscribers: [],
   migrations: [],
 });
+
+export default async function createDatabaseConnection(): Promise<void> {
+  return new Promise((resolve) => {
+    logger.info('🔌 Creating database connection...');
+    const createDatabaseConnectionInterval = setInterval(async () => {
+    
+      try {
+        await AppDataSource.initialize();
+        logger.info('✔️  Database connection is ready');
+        clearInterval(createDatabaseConnectionInterval);
+        resolve();
+      } catch (error: any) {
+        logger.error(error);
+      }
+
+    }, 5000);
+  });
+}
