e51276b9274b915d858c2ca466d36ac3721711ad
David del Puerto <code@daviddelpuerto.dev>
2022-09-30 00:31:46 +0200

    Implement exists method

--- src/modules/Users/infrastructure/repositories/MySqlUsersRepository.ts
+++ src/modules/Users/infrastructure/repositories/MySqlUsersRepository.ts
@@ -10,4 +10,9 @@ export default class MySqlUsersRepository implements UsersRepository {
   async save(email: string, password: string) {
     return this.userEntity.save({ email, password });
   }
+
+  async exists(email: string) {
+    const userRecord = await this.userEntity.findOneBy({ email });
+    return !!userRecord;
+  }
 }
